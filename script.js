a52bcd9b8bc21c9590a7aa3a7e79876c4131a506
Darren George Outram <doutram1027@gmail.com>
2016-06-10 18:56:18 +0100

    final

---
+++ script.js
@@ -0,0 +1,16 @@
+//this is the code for the smooth scroll//
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1500);
+                return false;
+            }
+        }
+    });
+});
+//this is where it ends//
